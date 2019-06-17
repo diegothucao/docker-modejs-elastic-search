a0c70427f9017a0869afb17947e11f8de15110fd
Thu CAO <cao.trung.thu@gmail.com>
2019-06-17 14:43:00 +0700

    update name

--- src/app.js
+++ src/app.js
@@ -1,40 +1,18 @@
 import cors from 'cors'
 import { urlencoded, json } from 'body-parser'
 import dotenv from 'dotenv'
+import express from 'express'
 
 dotenv.load()
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
 
-app.use(urlencoded({ extended: true, limit: '500mb' }))
-app.use(json({ extended: true, limit: '500mb' }))
+const app = express()
+app.use(urlencoded({ extended: true, limit: '500mb'}))
+app.use(json({ extended: true, limit: '500mb'}))
 app.use(cors())
 
-http.listen(process.env.PORT)
-
 app.get('/', (_, res) => {
-	res.send('Diego Cao: Hello')
-})
+	res.send('Diego Cao: Hello RabbitMQ')
+  })
 
-io.on('connection', (socket) => {
-	var uid = ""
-	console.log('user connected')
-	socket.on('disconnect', () => {
-		console.log('user disconnected')
-	})
-
-	socket.on("user_join", (data) => {
-		uid = data
-		console.log('uid: ' + uid)
-		socket.broadcast.emit("user_join", data)
-	})
-
-	socket.on("chat_message", (data) => {
-		data.uid = uid
-		console.log('data: ')
-		console.log(data)
-		socket.emit("chat_message", data)
-	})
-
-})
+let server = app.listen(process.env.PORT || 8080)
+server.setTimeout(500000)
